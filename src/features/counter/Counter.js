197b8a6ed1d7f1a0f0eec3d90483478eba2979f4
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-07-22 10:38:29 -0500

    Create Counter component

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
+
+
 
 const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <section>
+                <p>{count}</p>
+                <div>
+                    <button onClick={() => dispatch(increment())}>+</button>
+                    <button onClick={() => dispatch(decrement())}>-</button>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Counter
