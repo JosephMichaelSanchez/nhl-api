081161a1071c9d91851813a9af23c79d530356e5
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-07-22 11:27:37 -0500

    Add reset and incrementByAmount reducers to counterSlice

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { increment, decrement, reset, incrementByAmount } from './counterSlice';
+import { useState } from 'react';
 
 
 
@@ -8,6 +9,15 @@ const Counter = () => {
 
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
+
+    const [incrementAmount, setIncrementAmount] = useState(0);
+
+    const addValue = Number(incrementAmount) || 0;
+
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
 
     return (
         <div>
@@ -17,6 +27,16 @@ const Counter = () => {
                     <button onClick={() => dispatch(increment())}>+</button>
                     <button onClick={() => dispatch(decrement())}>-</button>
                 </div>
+                <input
+                    type='text'
+                    value={incrementAmount}
+                    onChange={(e) => setIncrementAmount(e.target.value)}
+                />
+                <div>
+                    <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+                    <button onClick={resetAll}>Reset</button>
+                </div>
+
             </section>
         </div>
     )
